e01a13a7bee3d8ac222ed72e459d88cde45f97a4
makotosakahara <makoto.sakahara@gmail.com>
2017-07-25 00:23:51 +0900

    [change] affilication.

--- home/.vim/template/template.py
+++ home/.vim/template/template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Author: Makoto Sakahara (Twitter: makotosakahara) <makoto.sakahara@gmail.com>
-# License: MIT License
+# Author:Makoto Sakahara <makoto.sakahara@gmail.com>
+# License:MIT License
