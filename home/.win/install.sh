2da2aeeb9b10b3f40c68a94c40ec3d718f75391d
makotosakahara <makoto.sakahara@gmail.com>
2018-03-23 14:10:10 +0900

    [add] one dark color scheme for mintty.

--- home/.win/install.sh
+++ home/.win/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp $HOME/.win/.minttyrc /mnt/c/Users/Makoto/AppData/Local/wsltty/home/makoto/
-cp $HOME/.win/.latexmkrc /mnt/c/Users/Makoto/
+cp $HOME/.win/.minttyrc /mnt/c/Users/makoto/AppData/Local/wsltty/home/makoto/
+cp $HOME/.win/.latexmkrc /mnt/c/Users/makoto/
