b39a5df10542f96c60e9e8d71ac225eb07747e14
makotosakahara <makoto.sakahara@gmail.com>
2019-04-08 16:56:27 +0900

    [fix] colors

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,1 +1,8 @@
 set fish_greeting
+
+set fish_color_autosuggestion 767677  # medium_gray
+set fish_color_error ff5458           # dark_red
+set fish_color_end 63f2f1             # dark cyan
+set fish_color_command 65b2ff         # dark_blue
+set fish_color_param 91ddff           # blue
+set fish_color_operator aaffe4        # cyan
