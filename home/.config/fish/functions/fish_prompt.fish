f07d2200191a53201700532c0fa2ee2271dcfd16
makotosakahara <makoto.sakahara@gmail.com>
2020-06-06 23:44:07 +0900

    [update] improve visibility

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
-    echo (set_color brred)"$USER"(set_color normal)' at '(set_color brmagenta)(prompt_hostname)(set_color normal)' in '(set_color brgreen)(prompt_pwd)' '
-    echo -n (set_color normal)↪' '
+    echo -n (set_color brgreen)(prompt_pwd)' '
+    set_color normal
 end
