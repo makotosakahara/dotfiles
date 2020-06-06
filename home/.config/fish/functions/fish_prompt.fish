73d0bb2b2ac54579d4c0e5e84d6ccca9fea5c0e8
makotosakahara <makoto.sakahara@gmail.com>
2020-06-06 11:46:05 +0900

    [update] improve visibility

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,14 +1,4 @@
 function fish_prompt
-    if test -n "$SSH_TTY"
-        echo -n (set_color red)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
-
-    echo (set_color blue)(prompt_pwd)
-
-    set_color -o
-    if test "$USER" = 'root'
-        echo -n (set_color red)'# '
-    end
-    echo -n (set_color green)\ufb0c' '
-    set_color normal
+    echo (set_color brred)"$USER"(set_color normal)' at '(set_color brmagenta)(prompt_hostname)(set_color normal)' in '(set_color brgreen)(prompt_pwd)' '
+    echo -n (set_color normal)↪' '
 end
