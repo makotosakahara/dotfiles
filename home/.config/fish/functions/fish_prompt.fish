85168510552afca9ef6ecb0fd3bcfe00e139381a
makotosakahara <makoto.sakahara@gmail.com>
2022-09-03 07:09:15 +0900

    [Upgrade]

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    echo (set_color magenta)"$USER"' '(prompt_hostname)' 🐍 '"$CONDA_DEFAULT_ENV"
-    echo -n (set_color green)(prompt_pwd)' '
+    echo (set_color brgreen)"$USER"'@'(prompt_hostname)' 🐍'"$CONDA_DEFAULT_ENV"
+    echo -n (set_color brgreen)(prompt_pwd)' '
     set_color normal
 end
