cd591bcb9f642d28c40462ff2977728f2d3c08c7
makotosakahara <makoto.sakahara@gmail.com>
2022-09-03 05:46:55 +0900

    [Update] Use official tokyonight fish theme.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,11 +1,38 @@
 set fish_greeting
 
-# Tokyo Night
-set fish_color_autosuggestion 565f89    # Comments
-set fish_color_error f7768e             # Terminal Red
-set fish_color_end 7dcfff               # Terminal Cyan
-set fish_color_command 7aa2f7           # Terminal Blue
-set fish_color_param 7aa2f7             # Terminal Blue
-set fish_color_operator 7dcfff          # Terminal Cyan
+# https://github.com/folke/tokyonight.nvim/blob/main/extras/fish_tokyonight_night.fish
+# TokyoNight Color Palette
+set -l foreground c0caf5
+set -l selection 33467C
+set -l comment 565f89
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
 
 eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
