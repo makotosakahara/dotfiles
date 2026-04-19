f4781ef8704698bd52040f01485b0ec1e19806ef
makotosakahara <makoto.sakahara@gmail.com>
2026-04-19 21:26:10 +0900

    fix(fzf): keybinding conflicts

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -17,5 +17,9 @@ abbr --add l lsd -la
 abbr --add lt lsd --tree
 functions --erase la ll
 
+# Prevent conflicts with patrickf1/fzf.fish.
+set -gx FZF_CTRL_R_COMMAND
+fzf --fish | source
+
 zoxide init fish | source
 starship init fish | source
