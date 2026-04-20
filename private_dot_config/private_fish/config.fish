d00341650d0a8f8affb2d1328a1d658d34061c44
makotosakahara <makoto.sakahara@gmail.com>
2026-04-20 17:21:51 +0900

    build(python): migrate from miniconda to uv

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,17 +1,5 @@
 set -U fish_greeting
 
-set path $HOME/miniconda3
-if test -f $path/bin/conda
-    eval $path/bin/conda shell.fish hook $argv | source
-else
-    if test -f $path/etc/fish/conf.d/conda.fish
-        . $path/etc/fish/conf.d/conda.fish
-    else
-        set -x PATH $path/bin $PATH
-    end
-end
-set --erase path
-
 abbr --add ls lsd
 abbr --add l lsd -la
 abbr --add lt lsd --tree
