65b5d3df641fe4d8c49a7a29531661b7122544a2
makotosakahara <makoto.sakahara@gmail.com>
2026-04-21 14:31:24 +0900

    feat(wezterm): disable inactive pane dimming

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,22 +4,26 @@ local config = wezterm.config_builder()
 if wezterm.target_triple:find("windows") then
 	config.default_domain = "WSL:Ubuntu"
 end
+
+-- Appearance
 config.window_background_opacity = 0.9
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.window_decorations = "RESIZE"
+-- Disable inactive pane dimming.
+config.inactive_pane_hsb = {
+	saturation = 1.0,
+	brightness = 1.0,
+}
 
--- Colorscheme.
-config.color_scheme = "Catppuccin Mocha"
-
--- Fonts.
-config.font = wezterm.font("Hack Nerd Font Mono")
-config.font_size = 16
-
--- Cursor.
+-- Text Cursor
 config.default_cursor_style = "BlinkingBar"
 config.force_reverse_video_cursor = true
 
--- Keybindings.
--- NOTE: No need to emulate tmux. LazyVim is sufficient.
+-- Color
+config.color_scheme = "Catppuccin Mocha"
+
+-- Font
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 16
 
 return config
