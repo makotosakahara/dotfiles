9900c9810f4b6bcf254d3126d15aa700c36198b5
makotosakahara <makoto.sakahara@gmail.com>
2026-04-24 19:59:16 +0900

    feat(ls): migrate from lsd to eza

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,9 +2,9 @@ set -U fish_greeting
 
 fish_config theme choose catppuccin-mocha --color-theme=dark
 
-abbr --add ls lsd
-abbr --add l lsd -la
-abbr --add lt lsd --tree
+abbr --add ls eza --icons --group-directories-first
+abbr --add l eza -la --color-scale --icons --group-directories-first
+abbr --add lt eza --tree --icons --group-directories-first
 functions --erase la ll
 
 mise activate fish | source
@@ -14,4 +14,5 @@ set -gx FZF_CTRL_R_COMMAND
 fzf --fish | source
 
 zoxide init fish | source
+
 starship init fish | source
