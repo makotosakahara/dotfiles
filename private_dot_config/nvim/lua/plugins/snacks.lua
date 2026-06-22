9b8501d35e2eaeffe34dbc4047d16014167fe5df
makotosakahara <14851677+makotosakahara@users.noreply.github.com>
2026-06-22 19:37:38 +0900

    feat(lazygit): centralize configuration

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,9 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    lazygit = {
+      configure = false,
+    },
     -- https://github.com/LazyVim/LazyVim/issues/4740#issuecomment-2466169614
     terminal = {
       win = {
